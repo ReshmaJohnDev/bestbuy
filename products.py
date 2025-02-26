6cf3e111464856809f59fb94102a98a437f861ae
Reshma John <reshma.chackola@gmail.com>
2025-02-26 11:26:15 +0100

    Updated the products.py  to achieve code optimization

--- products.py
+++ products.py
@@ -1,58 +1,68 @@
 class Product:
 
     def __init__(self, name, price, quantity):
-        if name and price and quantity:
-            if price >0 and quantity >0:
-                self.name = name
-                self.price = price
-                self.quantity = quantity
-                self.active = True
-        else :
-            raise Exception("Enter valid input")
+        """Initiator (constructor) method."""
+        if not name or price <= 0 or quantity < 0:
+            raise ValueError("Enter valid input: name must be non-empty,"
+                             " price > 0, and quantity >= 0")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = quantity > 0
+
 
     def get_quantity(self):
+        """Returns the quantity (int)"""
         return self.quantity
 
+
     def set_quantity(self, quantity):
+        """Setter function for quantity. If quantity reaches 0, deactivates the product."""
         self.quantity = quantity
         if self.quantity == 0:
             self.deactivate()
         else :
             self.activate()
 
-    def is_active(self):
 
+    def is_active(self):
+        """Returns True if the product is active, otherwise False."""
         if self.active:
             return True
-        else :
-            return False
+
 
     def deactivate(self):
+        """Deactivates the product."""
         self.active = False
 
+
     def activate(self):
+        """Activates the product."""
         self.active = True
 
 
     def show(self):
-        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity} "
+        """Returns a string that represents the product"""
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
 
     def buy(self, quantity):
-        if quantity:
-            total_price = float(self.price * quantity)
-            self.quantity -= quantity
-            self.set_quantity(self.quantity)
-            return total_price
+        """
+        Buys a given quantity of the product.
+        Returns the total price of the purchase.
+        Updates the quantity of the product.
+        """
+        if quantity <= 0:
+            raise ValueError("Enter a valid quantity (must be greater than 0)")
 
-        else :
-            raise Exception("Enter valid input")
+        if quantity > self.quantity:
+            raise ValueError("Error while making order! Quantity larger than what exists")
 
-
-
-
-
-
-
+        total_price = self.price * quantity
+        self.quantity -= quantity
+        self.set_quantity(self.quantity)
+        return total_price
 
 
 
